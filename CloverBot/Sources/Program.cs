46afb4d5d9cbbfac76b1156e91f61a67ef2b40d7
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2022-07-06 01:12:40 +0900

    created project

---
+++ CloverBot/Sources/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CloverBot.Boot;
+
+namespace CloverBot.Script {
+    public class Program {
+        private CloverBot cloverBot;
+
+        private static readonly string configPath = Path.Combine("data", "botConfig.json");
+
+        // Botの起動処理
+        static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
+        public Task MainAsync() { 
+            BotConfigManager.setJsonConfig(configPath);
+
+            cloverBot = new CloverBot();
+            return Task.CompletedTask;
+        }
+    }
+}
