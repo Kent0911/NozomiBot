40131e899c682234be98b0d1aab264e7a62568c7
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2022-07-06 01:13:38 +0900

    Bot Config

---
+++ CloverBot/Sources/Boot/BotConfigManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using CloverBot.Config;
+
+namespace CloverBot.Boot {
+    internal class BotConfigManager {
+        private static BotConfigData configData;
+
+        public static string token { get; private set; } = string.Empty;
+        public static string SQLConnectionString { get; private set; } = string.Empty;
+        public static void setJsonConfig (string _path) {
+            var jsonData = File.ReadAllText (_path);
+            configData = JsonSerializer.Deserialize<BotConfigData>(jsonData);
+            token = configData.discordConfig.token;
+
+            SQLConnectionString = $"server = { configData.databaseConfig.host }; " +
+                $" port = { configData.databaseConfig.port }; " +
+                $" user = { configData.databaseConfig.user }; " +
+                $" password = { configData.databaseConfig.password }; " +
+                $" database = { configData.databaseConfig.database }; " +
+                $" sslMode = { configData.databaseConfig.sslMode }";
+        }
+    }
+}
