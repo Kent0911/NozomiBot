065f2857f539079b68ff2754cb75e8aa3533c40c
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2022-07-11 23:05:18 +0900

    create CloverBotCommands.cs

---
+++ CloverBot/Sources/Config/CloverBotCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloverBot.Commands {
+    internal class CloverBotCommands {
+        public enum Commands {
+            SETUP,
+            ADD_MEMBER,
+            PHYSICAL_ATTACK,
+            MAGIC_ATTACK,
+            CARRY_OVER,
+            HELP,
+        }
+
+        private static Dictionary<String, Commands> dicCommands = new Dictionary<String, Commands>() {
+            { "setup", Commands.SETUP },
+            { "add_member", Commands.ADD_MEMBER },
+            { "physical_attack", Commands.PHYSICAL_ATTACK },
+            { "magic_attack", Commands.MAGIC_ATTACK },
+            { "carry_over", Commands.CARRY_OVER },
+            { "help", Commands.HELP }
+        };
+        private int GetCommandsValue(string _str) {
+            int value = -1;
+            foreach (var c in dicCommands) {
+                if (string.Equals(_str, c.Key)) value = (int)c.Value;
+            }
+            return value;
+        }
+    }
+}
